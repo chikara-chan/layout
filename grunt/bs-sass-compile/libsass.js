425c5e9501d449c6d8d943b1d429dc551db64bb2
chikara <chenhongtu@51xianqu.net>
2017-01-10 14:48:49 +0800

    Grunt build task run

--- grunt/bs-sass-compile/libsass.js
+++ grunt/bs-sass-compile/libsass.js
@@ -10,13 +10,14 @@ module.exports = function configureLibsass(grunt) {
       },
       core: {
         files: {
-          'dist/css/<%= pkg.name %>.css': 'scss/<%= pkg.name %>.scss'
+          // 'dist/css/<%= pkg.name %>.css': 'scss/<%= pkg.name %>.scss'
         }
       },
       extras: {
         files: {
           'dist/css/<%= pkg.name %>-grid.css': 'scss/<%= pkg.name %>-grid.scss',
-          'dist/css/<%= pkg.name %>-reboot.css': 'scss/<%= pkg.name %>-reboot.scss'
+          'dist/css/<%= pkg.name %>-reboot.css': 'scss/<%= pkg.name %>-reboot.scss',
+          'dist/css/<%= pkg.name %>-flex.css': 'scss/<%= pkg.name %>-flex.scss'
         }
       }
     }
